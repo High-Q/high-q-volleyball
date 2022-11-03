46690d655efb8bf4eef3f1601e25a15be29f215c
南翔太郎 <mshotaro@minamishoutarounoMacBook-puro.local>
2022-11-03 17:28:59 +0900

    server.js deleted

--- server.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var serveStatic = require("serve-static")
-var path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-var port = process.env.PORT || 80;
-app.listen(port);
