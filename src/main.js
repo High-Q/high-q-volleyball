627c7716fe4c10f687e16843294ff3e578a29d73
南翔太郎 <mshotaro@minamishoutarounoMacBook-puro.local>
2022-11-23 19:57:35 +0900

    install ga4 gtm

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import VueGtm from "@gtm-support/vue2-gtm";
 
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
   render: (h) => h(App),
+  VueGtm: {
+    id: "GTM-WNNF9RP",
+  },
 }).$mount("#app");
