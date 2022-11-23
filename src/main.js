3f4108f3c85e011c5b5a8f30c0d2cf6a285f87fd
南翔太郎 <mshotaro@minamishoutarounoMacBook-puro.local>
2022-11-24 00:52:26 +0900

    ga4 tag manager added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
+import VueGtm from 'vue-gtm';
 
 Vue.config.productionTip = false;
 
@@ -8,3 +9,8 @@ new Vue({
   vuetify,
   render: (h) => h(App),
 }).$mount("#app");
+
+Vue.use(VueGtm, {
+  id: 'GTM-WNNF9RP',
+});
+
