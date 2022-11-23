a902cf2b56bfbef130d0d960471c54bb5d4af588
南翔太郎 <mshotaro@minamishoutarounoMacBook-puro.local>
2022-11-24 00:38:44 +0900

    bug fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,47 +1,25 @@
 <template>
   <!-- <v-card height="400px"> -->
-  <v-footer padless color="white">
+  <v-footer min-height="280" class="footer" height="" padless color="white">
     <v-container fluid>
-      <v-row no-gutters>
+      <v-row align-content="center" justify="center" justify-md="start">
         <v-col cols="12" md="4">
-          <v-img height="180" src="../assets/footer.png">
-            <v-container fluid fill-height>
-              <v-row align-content="center" justify="center">
-                <v-col cols="12">
-                  <h3 class="footer-title">High Q</h3>
-                </v-col>
-                <v-btn
-                  class="mr-4"
-                  width="44"
-                  height="44"
-                  min-width="44"
-                  min-height="44"
-                  color="#6A96A4"
-                  href="https://twitter.com/c8w5y"
-                >
-                  <v-icon size="44" color="white" large>mdi-twitter</v-icon>
-                </v-btn>
-                <!-- <v-btn
-                  class="mr-4"
-                  width="44"
-                  height="44"
-                  min-width="44"
-                  min-height="44"
-                  color="#6A96A4"
-                  icon
-                  href="https://www.instagram.com/hiyori0104/"
-                >
-                  <v-icon size="58">mdi-instagram</v-icon>
-                </v-btn> -->
-              </v-row>
-            </v-container>
-          </v-img>
-        </v-col>
-        <v-col cols="4" class="mx-n2">
-          <v-img height="180" src="../assets/footer.png"> </v-img>
-        </v-col>
-        <v-col cols="4">
-          <v-img height="180" src="../assets/footer.png"> </v-img>
+          <p class="text-h5 text-md-h3 text-center footer-title">High Q</p>
+          <v-col>
+            <v-row justify="center">
+              <v-btn
+                class="pa-0"
+                height="24"
+                width="24"
+                min-width="24"
+                min-height="24"
+                color="#6A96A4"
+                href="https://twitter.com/c8w5y"
+              >
+                <v-icon size="24" color="white">mdi-twitter</v-icon>
+              </v-btn>
+            </v-row>
+          </v-col>
         </v-col>
       </v-row>
     </v-container>
@@ -71,5 +49,9 @@ export default {
   text-align: center;
   font-size: 40px;
   font-weight: bold;
+}
+.footer {
+  background-image: url("../assets/footer.png");
+  background-repeat: repeat-x;
 }
 </style>
