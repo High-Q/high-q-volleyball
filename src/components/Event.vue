bf271920246a97dc698a556c34925c14232b5cf9
南翔太郎 <mshotaro@minamishoutarounoMacBook-puro.local>
2023-05-09 23:00:15 +0900

    Simple API Event Callender

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -1,216 +1,108 @@
 <template>
-  <v-container fluid fill-height>
-    <v-row justify="center" class="pa-md-16">
-      <SubTitle title="Event"></SubTitle>
-    </v-row>
-    <v-row class="px-md-16">
-      <v-container>
-        <v-sheet height="64">
-          <v-toolbar flat>
-            <v-btn
-              outlined
-              class="pa-0"
-              color="grey darken-2"
-              @click="setToday"
-            >
-              Today
-            </v-btn>
-            <v-btn
-              fab
-              text
-              small
-              class="pa-0"
-              color="grey darken-2"
-              @click="prev"
-            >
-              <v-icon small>
-                mdi-chevron-left
-              </v-icon>
-            </v-btn>
-            <v-btn
-              fab
-              text
-              smal
-              class="pa-0"
-              color="grey darken-2"
-              @click="next"
-            >
-              <v-icon small>
-                mdi-chevron-right
-              </v-icon>
-            </v-btn>
-            <v-toolbar-title class="pa-0" v-if="$refs.calendar">
-              {{ $refs.calendar.title }}
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-menu bottom right>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  outlined
-                  small
-                  class="pa-0"
-                  color="grey darken-2"
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  <span>{{ typeToLabel[type] }}</span>
-                  <v-icon right>
-                    mdi-menu-down
-                  </v-icon>
-                </v-btn>
-              </template>
-              <v-list>
-                <v-list-item @click="type = 'day'">
-                  <v-list-item-title>Day</v-list-item-title>
-                </v-list-item>
-                <v-list-item @click="type = 'week'">
-                  <v-list-item-title>Week</v-list-item-title>
-                </v-list-item>
-                <v-list-item @click="type = 'month'">
-                  <v-list-item-title>Month</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-          </v-toolbar>
-        </v-sheet>
-        <v-sheet height="600">
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-sheet>
           <v-calendar
             ref="calendar"
             v-model="focus"
             color="primary"
             :events="events"
             :event-color="getEventColor"
-            :type="type"
             @click:event="showEvent"
             @click:more="viewDay"
             @click:date="viewDay"
-            @change="updateRange"
           ></v-calendar>
         </v-sheet>
-      </v-container>
+      </v-col>
     </v-row>
+    <v-dialog v-model="dialog" max-width="290">
+      <v-card>
+        <v-card-title class="headline">{{ selectedEvent.name }}</v-card-title>
+        <v-card-text>
+          <div>
+            <strong>開始時間:</strong> {{ formatDate(selectedEvent.start) }}
+          </div>
+          <div>
+            <strong>終了時間:</strong> {{ formatDate(selectedEvent.end) }}
+          </div>
+          <div><strong>詳細:</strong> {{ selectedEvent.details }}</div>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="dialog = false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
+
 <script>
-import SubTitle from "./SubTitle.vue";
+import axios from "axios";
 export default {
-  name: "Event",
-  components: {
-    SubTitle,
+  data() {
+    return {
+      focus: "",
+      dialog: false,
+      selectedEvent: {},
+      events: [
+        {
+          name: "ワークショップ",
+          start: "2023-05-15T10:00:00",
+          end: "2023-05-15T12:00:00",
+          details: "ワークショップの説明がここに入ります。",
+          color: "blue",
+        },
+        {
+          name: "セミナー",
+          start: "2023-05-20T14:00:00",
+          end: "2023-05-20T16:00:00",
+          details: "セミナーの説明がここに入ります。",
+          color: "green",
+        },
+        {
+          name: "ネットワーキングイベント",
+          start: "2023-05-25T18:00:00",
+          end: "2023-05-25T21:00:00",
+          details: "ネットワーキングイベントの説明がここに入ります。",
+          color: "red",
+        },
+      ],
+    };
   },
-  data: () => ({
-    focus: "",
-    type: "month",
-    typeToLabel: {
-      month: "Month",
-      week: "Week",
-      day: "Day",
-    },
-    selectedEvent: {},
-    selectedElement: null,
-    selectedOpen: false,
-    events: [],
-  }),
-  mounted() {
-    this.$refs.calendar.checkChange();
-  },
+  // mounted() {
+  //   this.fetchEvents();
+  // },
   methods: {
-    viewDay({ date }) {
-      this.focus = date;
-      this.type = "day";
-    },
-    setToday() {
-      this.focus = "";
-    },
-    prev() {
-      this.$refs.calendar.prev();
-    },
-    next() {
-      this.$refs.calendar.next();
-    },
-    updateRange() {
-      const events = [];
-      for (let i = 0; i < 1; i++) {
-        //Eventの数だけループを回す
-        events.push(
-          {
-            name: "バレー会",
-            start: new Date("2022-10-30T18:00:00"), // 開始時刻
-            end: new Date("2022-10-30T21:30:00"), // 終了時刻
-            color: "blue",
-            details: new Date("2022-10-30T18:00:00"),
-            timed: true, // 終日ならfalse
-          },
-          {
-            name: "バレー会",
-            start: new Date("2023-01-14T09:00:00"), // 開始時刻
-            end: new Date("2023-01-14T12:00:00"), // 終了時刻
-            color: "blue",
-            details: new Date("2023-01-14T09:00:00"),
-            timed: true, // 終日ならfalse
-          },
-          {
-            name: "バレー会",
-            start: new Date("2023-02-05T18:00:00"), // 開始時刻
-            end: new Date("2023-02-05T21:30:00"), // 終了時刻
-            color: "blue",
-            details: new Date("2023-02-05T18:00:00"),
-            timed: true, // 終日ならfalse
-          },
-          {
-            name: "バレー会",
-            start: new Date("2023-02-18T09:00:00"), // 開始時刻
-            end: new Date("2023-02-18T12:00:00"), // 終了時刻
-            color: "blue",
-            details: new Date("2023-02-18T09:00:00"),
-            timed: true, // 終日ならfalse
-          },
-          {
-            name: "バレー会",
-            start: new Date("2023-03-26T18:00:00"), // 開始時刻
-            end: new Date("2023-03-26T21:00:00"), // 終了時刻
-            color: "blue",
-            details: new Date("2023-03-26T18:00:00"),
-            timed: true, // 終日ならfalse
-          },
-          {
-            name: "バレー会",
-            start: new Date("2023-04-23T13:00:00"), // 開始時刻
-            end: new Date("2023-04-23T17:00:00"), // 終了時刻
-            color: "blue",
-            details: new Date("2023-04-23T13:00:00"),
-            timed: true, // 終日ならfalse
-          },
-          {
-            name: "バレー会",
-            start: new Date("2023-05-13T18:00:00"), // 開始時刻
-            end: new Date("2023-05-13T20:00:00"), // 終了時刻
-            color: "blue",
-            details: new Date("2023-05-13T18:00:00"),
-            timed: true, // 終日ならfalse
-          },
-          {
-            name: "バレー会",
-            start: new Date("2023-05-27T16:00:00"), // 開始時刻
-            end: new Date("2023-05-27T18:00:00"), // 終了時刻
-            color: "blue",
-            details: new Date("2023-05-27T16:00:00"),
-            timed: true, // 終日ならfalse
-          },
-          {
-            name: "バレー会",
-            start: new Date("2023-06-10T16:00:00"), // 開始時刻
-            end: new Date("2023-06-10T18:00:00"), // 終了時刻
-            color: "blue",
-            details: new Date("2023-06-10T16:00:00"),
-            timed: true, // 終日ならfalse
-          }
-        );
-      }
-      this.events = events;
-    },
     getEventColor(event) {
       return event.color;
+    },
+    showEvent({ event }) {
+      console.log(event.start);
+      this.selectedEvent = event;
+      this.dialog = true;
+    },
+    viewDay({ date }) {
+      this.focus = date;
+      this.$refs.calendar.checkChange();
+    },
+    async fetchEvents() {
+      try {
+        const response = await axios.get("YOUR_API_URL");
+        this.events = response.data.events;
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      }
+    },
+    formatDate(date) {
+      const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      };
+      return new Date(date).toLocaleString("ja-JP", options);
     },
   },
 };
