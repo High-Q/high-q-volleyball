02bca03c91c10b4b6cdd5f4743ac21b3f5bf9017
南翔太郎 <mshotaro@minamishoutarounoMacBook-puro.local>
2023-05-11 00:12:07 +0900

    hummer config of direction_horizontal

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
-import VueGtm from 'vue-gtm';
-import { VueHammer } from 'vue2-hammer';
+import VueGtm from "vue-gtm";
+import { VueHammer } from "vue2-hammer";
+import Hammer from 'hammerjs';
 
+VueHammer.config.swipe = {
+  direction: Hammer.DIRECTION_HORIZONTAL
+};
 Vue.config.productionTip = false;
+
 Vue.use(VueHammer);
 
 new Vue({
@@ -13,5 +18,5 @@ new Vue({
 }).$mount("#app");
 
 Vue.use(VueGtm, {
-  id: 'GTM-WNNF9RP',
+  id: "GTM-WNNF9RP",
 });
